abb3f16435bd51958117cd6acd5ec92a41e731b6
SimaSKumbhar <121923172+SimaSKumbhar@users.noreply.github.com>
2023-08-05 13:47:57 +0530

    Update Signup.jsx

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css'
 import axios from 'axios'
 import {Link, useNavigate} from 'react-router-dom'
+import './Signup.css'
 
 function Signup() {
     const [name, setName] = useState()
@@ -19,13 +20,13 @@ function Signup() {
     }
 
   return (
-    <div className="d-flex justify-content-center align-items-center bg-secondary vh-100">
-      <div className="bg-white p-3 rounded w-25">
-        <h2>Register</h2>
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <div className="sign-up p-3 rounded w-25">
+        <h2 style={{color:"white"}}>Register</h2>
         <form onSubmit={handleSubmit}>
         <div className="mb-3">
             <label htmlFor="name">
-              <strong>Name</strong>
+              <strong style={{color:"white"}}>Name</strong>
             </label>
             <input
               type="text"
@@ -38,7 +39,7 @@ function Signup() {
           </div>
           <div className="mb-3">
             <label htmlFor="date-of-birth">
-              <strong>Date Of Birth</strong>
+              <strong style={{color:"white"}}>Date Of Birth</strong>
             </label>
             <input
               type="date"
@@ -51,7 +52,7 @@ function Signup() {
           </div>
           <div className="mb-3">
             <label htmlFor="email">
-              <strong>Email</strong>
+              <strong style={{color:"white"}}>Email</strong>
             </label>
             <input
               type="email"
@@ -64,7 +65,7 @@ function Signup() {
           </div>
           <div className="mb-3">
             <label htmlFor="password">
-              <strong>Password</strong>
+              <strong style={{color:"white"}}>Password</strong>
             </label>
             <input
               type="password"
@@ -74,13 +75,13 @@ function Signup() {
               onChange={(e) => setPassword(e.target.value)}
             />
           </div>
-          <button type="submit" className="btn btn-success w-100 rounded-0">
+          <button type="submit" className="btn btn-danger w-100 rounded-0">
             Register
           </button>
           </form>
-          <p>Already Have an Account</p>
+          <p  style={{color:"white"}}> Already Have an Account</p>
           <Link to="/login">
-           <button type="submit" className="btn btn-primary w-100 rounded-0">Login</button>
+           <button type="submit" className="btn btn-danger w-100 rounded-0">Login</button>
           </Link>
         
       </div>
